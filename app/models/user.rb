cefc7581609374c4ebd81f1f699b30b7fe01d3fc
Tanat Cheurthai <naepanda@hotmail.com>
2021-01-06 12:01:44 +0700

    Add user association

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   validates :username, length: { in: 4..12 },
                        presence: true,
                        uniqueness: true
+
+  has_many :posts
 end
